97d62e8cc311990ce1509b8eee7d1a9ba358872b
DerpNuggetBoi <92122910+DerpNuggetBoi@users.noreply.github.com>
2021-10-12 17:49:24 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("According to all known laws of aviation, the bee should not be able to fly.");
+  }
+}
+/*This is a comment*/
+//This is also a comment
+/*Java Variable Types
+You have 5 types of VARIABLES IN Java
+Boolean: True/False, only 2 states - boolean
+Intergers: Int , always WHOLE numbers
+Strings: Text , ex "Hello"
+Floats: Decimals, ex "19.999"
+Char: Single characters, ex "A", "B", WRITE A CODE THAT GIVES YOU HOW MANY CHARACTERS ARE IN "Pneumonoultramicroscopicsilicovolcanoconiosis"
+
+
+In math x = 4 means 4 = x
+In programming x = 4 {4 is assigned to x}
+
+x = "Patty"
+b = "Tomato"
+c = "Cheese"
+d = "Bread"
+e = "Lettuce"
+T = "Ketchup"
+g = "Mayo"
+
+So the template is: type variable = value;
+*/
